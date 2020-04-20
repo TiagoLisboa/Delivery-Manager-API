a0005dd4cdebcdddcafb0788496b8a76181c6ae4
Tiago Lisboa <tiago.caio.ol@gmail.com>
2020-04-20 17:17:18 -0300

    create address

---
+++ app/Address.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Address extends Model
+{
+  protected $fillable = [
+    'address',
+    'complement',
+    'neighborhood',
+    'city',
+    'state',
+    'CEP'
+  ];
+}
---
+++ database/migrations/2020_04_20_192950_create_addresses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('address')->required();
+            $table->string('complement')->nullable();
+            $table->string('neighborhood')->required();
+            $table->string('city')->required();
+            $table->string('state')->required();
+            $table->string('CEP')->required();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
